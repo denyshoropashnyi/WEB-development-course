9b2ec3973721a59e6cf84aaf01e935fa64c58ec8
Denys Horopashnyi <44978023+denyshoropashnyi@users.noreply.github.com>
2019-07-09 15:44:23 +0300

    - add getCenturyNumber() - add isCenturyNumberValid()

---
+++ Javascript_ES5_Course/Homework/Lesson_1/DenysHoropashnyi.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function getCenturyNumber(number) {
+    if (isCenturyNumberValid(number)) {
+        console.log(Math.ceil(number / 100));
+    } else {
+        console.log(' Year value is incorrect.\n Please, try again. ')
+    }
+}
+
+function isCenturyNumberValid(number) {
+    if (number >= 1 && number <= 2017) {
+        return number;
+    }
+}
+
+getCenturyNumber(1700);
+getCenturyNumber(19505);
